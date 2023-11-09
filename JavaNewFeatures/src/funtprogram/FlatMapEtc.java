8d770fb9d6ae082cb2efad7319fe03687bed68a8
egelirli <egelirli@yahoo.com>
2023-11-09 22:32:41 +0300

    createTuplesWithSameLength

--- JavaNewFeatures/src/funtprogram/FlatMapEtc.java
+++ JavaNewFeatures/src/funtprogram/FlatMapEtc.java
@@ -14,19 +14,39 @@ public class FlatMapEtc {
 		  createTuplesWithSameLength(courses);
 	}
 
-	private static void createTuplesWithSameLength(List<String> courses) {
+	private static void createTuplesWithSameLength(List<String> courses1) {
 		List<String> courses2 = List.of("Spring", "Spring Boot", "API" , "Microservices",
 				"AWS", "PCF","Azure", "Docker", "Kubernetes");
 		
+		System.out.println("Char stream 1: " );
+			courses1.stream().
+			     map(course -> course.split("")).
+			     forEach(carr ->  System.out.println(Arrays.toString(carr)));
+			     //collect(Collectors.toList())
+//		);
+				
+		System.out.println("Char stream : " + 
+		           courses1.stream().map(course -> course.split("")).flatMap(Arrays::stream).collect(Collectors.toList())
+		);
+
 		
-		courses.stream().map(course -> course.split("")).flatMap(Arrays::stream).collect(Collectors.toList());
-		System.out.println("Tuples: " +
-		  courses.stream().
-			flatMap(course -> courses2.stream().map(course2 -> List.of(course,course2)))
-			.filter(list -> !list.get(0).equals(list.get(1)))
-			.filter(list -> list.get(0).length() == list.get(1).length())
+//		System.out.println("---------");
+//		System.out.println("Tuples: " +
+//				  courses1.stream().
+//					flatMap(course -> courses2.stream().map(course2 -> List.of(course,course2)))
+//		);
+		
+		System.out.println("---------");
+		
+		System.out.println("Tuples 2: " +
+		  courses1.stream().
+			flatMap(course -> courses2.stream().
+					filter(course2 -> course2.length() == course.length()).
+					   map(course2 -> List.of(course,course2))
+			.filter(list -> !list.get(0).equals(list.get(1))))
 			.collect(Collectors.toList())
 		 ); 
+		//.filter(list -> list.get(0).length() == list.get(1).length())
 				
 	}
 	
